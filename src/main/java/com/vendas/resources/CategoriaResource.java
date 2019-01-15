0f7251d2f596947f3651d57061644e568ff370f1
Filipe Soares <filipecarlossoares@gmail.com>
2019-01-15 21:34:54 -0200

    Testanto primeira classe de dominio Categoria

--- src/main/java/com/vendas/resources/CategoriaResource.java
+++ src/main/java/com/vendas/resources/CategoriaResource.java
@@ -1,15 +1,23 @@
 package com.vendas.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vendas.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is working!";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1L, "Categoria 1");
+		Categoria cat2 = new Categoria(2L, "Categoria 2");
+
+		return Arrays.asList(cat1, cat2);
 	}
 }
