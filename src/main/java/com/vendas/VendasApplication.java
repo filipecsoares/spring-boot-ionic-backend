36d8ccf3143606ff876b5d5da2f8b82755122142
Filipe Soares <filipecarlossoares@gmail.com>
2019-01-17 19:20:04 -0200

    Inserindo dados de categoria no banco

--- src/main/java/com/vendas/VendasApplication.java
+++ src/main/java/com/vendas/VendasApplication.java
@@ -1,14 +1,31 @@
 package com.vendas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vendas.domain.Categoria;
+import com.vendas.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+
+}
