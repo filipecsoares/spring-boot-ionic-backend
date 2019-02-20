60bef72e5539f9b7aed0f4a713f50ac04aad5f88
Filipe Soares <filipecarlossoares@gmail.com>
2019-02-20 18:42:24 -0300

    Implementando autorizacao

--- src/main/java/com/vendas/config/SecurityConfig.java
+++ src/main/java/com/vendas/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.vendas.security.JWTAuthenticationFilter;
+import com.vendas.security.JWTAuthorizationFilter;
 import com.vendas.security.JWTUtil;
 
 @Configuration
@@ -47,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
